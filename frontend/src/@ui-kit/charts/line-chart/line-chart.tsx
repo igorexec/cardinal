8a26b5078ea101114951de984ee1aece28ffef10
Igor Cheliadinski <icheliadinski@gmail.com>
2019-08-12 15:41:32 -0400

    chg: fix x axis

--- frontend/src/@ui-kit/charts/line-chart/line-chart.tsx
+++ frontend/src/@ui-kit/charts/line-chart/line-chart.tsx
@@ -11,28 +11,28 @@ export class LineChart extends Component {
 
   componentDidMount(): void {
     const data: Score[] = [
-      {score: 60, date: new Date('2019-08-11T16:10:36.955Z')},
-      {score: 30, date: new Date('2019-08-12T16:10:36.955Z')},
-      {score: 20, date: new Date('2019-08-13T16:10:36.955Z')},
-      {score: 75, date: new Date('2019-08-14T16:10:36.955Z')},
-      {score: 92, date: new Date('2019-08-15T16:10:36.955Z')},
+      {score: 0, date: new Date('2019-08-11T00:00:00.000')},
+      {score: 100, date: new Date('2019-08-12T00:00:00.000')},
+      {score: 0, date: new Date('2019-08-13T00:00:00.000')},
+      {score: 100, date: new Date('2019-08-14T00:00:00.000')},
+      {score: 0, date: new Date('2019-08-15T00:00:00.000')},
+      {score: 100, date: new Date('2019-08-16T00:00:00.000')},
     ];
     const minMaxDates = d3.extent(data, d => d.date) as [Date, Date];
-    const xScale = d3.scaleLinear().domain(minMaxDates).range([0, 500]);
+    const xScale = d3.scaleTime().domain(minMaxDates).range([0, 1000]);
     const yScale = d3.scaleLinear().domain([100, 0]).range([0, 300]);
 
-    const dateFormat = d3.timeFormat('%Y-%m-%d') as () => string;
-    const makeXLines = d3.axisBottom(xScale).tickFormat(dateFormat);
+    const dateFormat = d3.timeFormat('%Y-%m-%d %HH-%MM') as () => string;
+    const makeXLines = d3.axisBottom(xScale).tickFormat(dateFormat).ticks(6);
     const makeYLines = d3.axisLeft(yScale);
 
-    d3.select(this.el.current).append('g').attr('transform', `translate(30, 310)`).call(makeXLines);
-    d3.select(this.el.current).append('g').attr('transform', 'translate(30, 10)').call(makeYLines);
+    d3.select(this.el.current).append('g').call(makeXLines);
+    d3.select(this.el.current).append('g').call(makeYLines);
 
     const line = d3.line<Score>().x(d => xScale(d.date)).y(d => yScale(d.score));
     d3.select(this.el.current)
       .append('path')
       .datum(data)
-      .attr('transform', 'translate(30, 10)')
       .attr('fill', 'none')
       .attr('stroke', 'black')
       .attr('stroke-width', '1')
@@ -42,7 +42,7 @@ export class LineChart extends Component {
   render() {
     return (
       <div>
-        <svg ref={this.el} width="100%" height={350} />
+        <svg ref={this.el} width="100%" height={300} />
       </div>
     );
   }
