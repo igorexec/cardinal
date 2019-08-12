205c5404c0503071688e26f58af7711e7e83f18e
Igor Cheliadinski <icheliadinski@gmail.com>
2019-08-12 18:37:42 -0400

    add: logic for multiple charts

--- frontend/src/@pages/pagespeed/chart.tsx
+++ frontend/src/@pages/pagespeed/chart.tsx
@@ -7,10 +7,31 @@ interface Props {
 }
 
 export const PageSpeedChart: React.FC<Props> = ({title}) => {
+  const lines = [{
+    data: [
+      {score: 0, date: new Date('2019-08-11T00:00:00.000')},
+      {score: 100, date: new Date('2019-08-12T00:00:00.000')},
+      {score: 0, date: new Date('2019-08-13T00:00:00.000')},
+      {score: 100, date: new Date('2019-08-14T00:00:00.000')},
+      {score: 0, date: new Date('2019-08-15T00:00:00.000')},
+      {score: 100, date: new Date('2019-08-16T00:00:00.000')},
+    ],
+    name: 'www.toryburch.com',
+  }, {
+    data: [
+      {score: 33, date: new Date('2019-08-11T00:00:00.000')},
+      {score: 71, date: new Date('2019-08-12T00:00:00.000')},
+      {score: 54, date: new Date('2019-08-13T00:00:00.000')},
+      {score: 81, date: new Date('2019-08-14T00:00:00.000')},
+      {score: 13, date: new Date('2019-08-15T00:00:00.000')},
+      {score: 77, date: new Date('2019-08-16T00:00:00.000')},
+    ],
+    name: 'www.toryburch1.com',
+  }];
   return (
     <section>
       <h1>{title}</h1>
-      <LineChart />
+      <LineChart data={lines} />
     </section>
   );
 };
--- frontend/src/@ui-kit/charts/line-chart/line-chart.tsx
+++ frontend/src/@ui-kit/charts/line-chart/line-chart.tsx
@@ -6,37 +6,42 @@ interface Score {
   date: Date;
 }
 
-export class LineChart extends Component {
+interface Props {
+  data: Array<{
+    name: string;
+    data: Score[];
+  }>;
+}
+
+export class LineChart extends Component<Props> {
   private el = React.createRef<SVGSVGElement>();
 
   componentDidMount(): void {
-    const data: Score[] = [
-      {score: 0, date: new Date('2019-08-11T00:00:00.000')},
-      {score: 100, date: new Date('2019-08-12T00:00:00.000')},
-      {score: 0, date: new Date('2019-08-13T00:00:00.000')},
-      {score: 100, date: new Date('2019-08-14T00:00:00.000')},
-      {score: 0, date: new Date('2019-08-15T00:00:00.000')},
-      {score: 100, date: new Date('2019-08-16T00:00:00.000')},
-    ];
-    const minMaxDates = d3.extent(data, d => d.date) as [Date, Date];
-    const xScale = d3.scaleTime().domain(minMaxDates).range([0, 1000]);
-    const yScale = d3.scaleLinear().domain([100, 0]).range([0, 300]);
+    const {data} = this.props;
 
-    const dateFormat = d3.timeFormat('%Y-%m-%d %HH-%MM') as () => string;
-    const makeXLines = d3.axisBottom(xScale).tickFormat(dateFormat).ticks(6);
+    const scores = data.reduce((acc, l) => {
+      return [...acc, ...l.data];
+    }, [] as Score[]);
+    const [minDate, maxDate] = d3.extent(scores, s => s.date) as [Date, Date];
+    const xScale = d3.scaleTime().domain([minDate, maxDate]).range([0, 1000]);
+    const yScale = d3.scaleLinear().domain([100, 0]).range([0, 300]);
+    const dateFormat = d3.timeFormat('%Y-%m-%d') as () => string;
+    const makeXLines = d3.axisBottom(xScale).tickFormat(dateFormat);
     const makeYLines = d3.axisLeft(yScale);
 
     d3.select(this.el.current).append('g').call(makeXLines);
     d3.select(this.el.current).append('g').call(makeYLines);
 
-    const line = d3.line<Score>().x(d => xScale(d.date)).y(d => yScale(d.score));
-    d3.select(this.el.current)
-      .append('path')
-      .datum(data)
-      .attr('fill', 'none')
-      .attr('stroke', 'black')
-      .attr('stroke-width', '1')
-      .attr('d', line);
+    data.forEach(l => {
+      const line = d3.line<Score>().x(d => xScale(d.date)).y(d => yScale(d.score));
+      d3.select(this.el.current)
+        .append('path')
+        .datum(l.data)
+        .attr('fill', 'none')
+        .attr('stroke', 'black')
+        .attr('stroke-width', '1')
+        .attr('d', line);
+    });
   }
 
   render() {
