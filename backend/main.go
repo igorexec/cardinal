9e5cce10c67e7260651a5398ae3b148468bc2900
Igor Cheliadinski <icheliadinski@gmail.com>
2019-08-13 20:11:14 -0400

    add: Add stacktrace check

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/icheliadinski/cardinal/cmd"
 	"github.com/jessevdk/go-flags"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 )
 
 type Opts struct {
@@ -50,4 +53,26 @@ func setupLog(dbg bool) {
 		return
 	}
 	lgr.Setup(lgr.Msec, lgr.LevelBraces)
+}
+
+func init() {
+	// catch SIGQUIT and print stack traces
+	sigChan := make(chan os.Signal)
+	go func() {
+		for range sigChan {
+			lgr.Printf("[INFO] SIGQUIT detected, dump:\n%s", getDump())
+		}
+	}()
+	signal.Notify(sigChan, syscall.SIGQUIT)
+}
+
+// getDump reads runtime stack and returns as a string
+func getDump() string {
+	maxSize := 5 * 1024 * 1024
+	stacktrace := make([]byte, maxSize)
+	length := runtime.Stack(stacktrace, true)
+	if length > maxSize {
+		length = maxSize
+	}
+	return string(stacktrace[:length])
 }
