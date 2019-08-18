52e5eba2d9776a4af64e3376d4231e4148806db0
Igor Cheliadinski <icheliadinski@gmail.com>
2019-08-18 19:45:24 -0400

    add: storage

--- backend/cmd/server.go
+++ backend/cmd/server.go
@@ -3,17 +3,33 @@ package cmd
 import (
 	"context"
 	"github.com/go-pkgz/lgr"
+	"github.com/go-pkgz/mongo"
 	"github.com/icheliadinski/cardinal/rest/api"
+	"github.com/icheliadinski/cardinal/store/engine"
+	"github.com/icheliadinski/cardinal/store/service"
 	"github.com/pkg/errors"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 )
 
 type ServerCommand struct {
+	Store StoreGroup `group:"store" namespace:"store" env-namespace:"STORE"`
+	Mongo MongoGroup `group:"mongo" namespace:"mongo" env-namespace:"MONGO"`
+
 	Port int `long:"port" env:"CARDINAL_PORT" default:"8080" description:"port"`
 	CommonOpts
+}
+
+type StoreGroup struct {
+	Type string `long:"type" env:"TYPE" description:"type of storage" choice:"mongo" default:"mongo"`
+}
+
+type MongoGroup struct {
+	URL string `long:"url" env:"url" description:"mongo url"`
+	DB  string `long:"db" env:"DB" default:"cardinal" description:"mongo database"`
 }
 
 type serverApp struct {
@@ -53,7 +69,17 @@ func (s *ServerCommand) newServerApp() (*serverApp, error) {
 	}
 	lgr.Printf("[INFO] root url=%s:%d", s.CardinalURL, s.Port)
 
+	storeEngine, err := s.makeDataStore()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make data store engine")
+	}
+
+	dataService := &service.DataStore{
+		Interface: storeEngine,
+	}
+
 	srv := &api.Rest{
+		DataServer:  dataService,
 		Version:     s.Revision,
 		CardinalURL: s.CardinalURL,
 	}
@@ -74,4 +100,28 @@ func (a *serverApp) run(ctx context.Context) error {
 
 	a.restSrv.Run(a.Port)
 	return nil
+}
+
+func (s *ServerCommand) makeDataStore() (result engine.Interface, err error) {
+	lgr.Printf("[INFO] make data store, type=%s", s.Store.Type)
+
+	switch s.Store.Type {
+	case "mongo":
+		mgServer, err := s.makeMongo()
+		if err != nil {
+			return result, errors.Wrap(err, "failed to create mongo server")
+		}
+		conn := mongo.NewConnection(mgServer, s.Mongo.DB, "")
+		result, err = engine.NewMongo(conn, 500, 100*time.Millisecond)
+	default:
+		return nil, errors.Errorf("unsupported store type %s", s.Store.Type)
+	}
+	return result, errors.Wrapf(err, "can't initialize data store")
+}
+
+func (s *ServerCommand) makeMongo() (result *mongo.Server, err error) {
+	if s.Mongo.URL == "" {
+		return nil, errors.New("no mongo URL provided")
+	}
+	return mongo.NewServerWithURL(s.Mongo.URL, 10*time.Second)
 }
---
+++ backend/store/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+import "github.com/icheliadinski/cardinal/store/engine"
+
+type DataStore struct {
+	Interface engine.Interface
+}
